4ccac72f676466c3319f8fec1f88e7539208fc23
astro-andrea <abracamonte@gradcenter.cuny.edu>
2024-08-14 15:41:51 -0400

    added extra lines to make functions work

--- functions.py
+++ functions.py
@@ -2,16 +2,21 @@
 # Replace the "raise NotImplementedError" line with the correct code!
    
 def print_the_instructors_name():
-    raise NotImplementedError
+    print ('Dax Felix')
+print_the_instructors_name()
 
 def print_the_number_of_this_room():
-    raise NotImplementedError
+    print('5328')
+print_the_number_of_this_room()
 
 def split_this_string_at_each_space(input_string):
     raise NotImplementedError
 
 def take_the_average_of_these_numbers(a, b):
-    raise NotImplementedError
+    import numpy
+    avg=numpy.mean[a b]
+    print(avg)
+
 
 def print_the_time_now_using_astropy():
     raise NotImplementedError
